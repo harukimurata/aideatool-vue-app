8b41a27acddee0a4b01f0dad99df2e537e187af2
harukimurata <havedream0108@gmail.com>
2024-09-30 15:52:44 +0900

    fix: 素材ロード処理

--- src/game/const/TextureKey.ts
+++ src/game/const/TextureKey.ts
@@ -1,6 +1,26 @@
 enum TextureKey {
   PushButton = 'pushButton',
-  PushHoverButton = 'pushHoverButton'
+  PushHoverButton = 'pushHoverButton',
+  SlotMachineOver = 'slot_machine_over',
+  SlotMachineUnder = 'slot_machine_under',
+  SlotStartA = 'slot_start_a',
+  SlotStartB = 'slot_start_b',
+  SlotButtonA = 'slot_button_a',
+  SlotButtonB = 'slot_button_b',
+  SlotButtonC = 'slot_button_c',
+  SlotButtonD = 'slot_button_d',
+  SlotButtonE = 'slot_button_e',
+  SlotButtonF = 'slot_button_f',
+  SlotNumber0 = 'slot_number_0',
+  SlotNumber1 = 'slot_number_1',
+  SlotNumber2 = 'slot_number_2',
+  SlotNumber3 = 'slot_number_3',
+  SlotNumber4 = 'slot_number_4',
+  SlotNumber5 = 'slot_number_5',
+  SlotNumber6 = 'slot_number_6',
+  SlotNumber7 = 'slot_number_7',
+  SlotNumber8 = 'slot_number_8',
+  SlotNumber9 = 'slot_number_9'
 }
 
 export default TextureKey
--- src/game/scenes/Preloader.ts
+++ src/game/scenes/Preloader.ts
@@ -10,11 +10,14 @@ export class Preloader extends Scene {
     //  We loaded this image in our Boot Scene, so we can display it here
     this.add.image(512, 384, 'background')
 
+    const gameWidth = this.scale.width
+    const gameHeight = this.scale.height
+
     //  A simple progress bar. This is the outline of the bar.
-    this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff)
+    this.add.rectangle(gameWidth / 2, gameHeight / 2, 468, 32).setStrokeStyle(1, 0xffffff)
 
     //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-    const bar = this.add.rectangle(512 - 230, 384, 4, 28, 0xffffff)
+    const bar = this.add.rectangle(gameWidth / 2 - 230, gameHeight / 2, 4, 28, 0xffffff)
 
     //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
     this.load.on('progress', (progress: number) => {
@@ -31,6 +34,26 @@ export class Preloader extends Scene {
     this.load.image('star', 'star.png')
     this.load.image(TextureKey.PushButton, 'image/push_button.png')
     this.load.image(TextureKey.PushHoverButton, 'image/push_h_button.png')
+    this.load.image(TextureKey.SlotMachineOver, 'image/slot/slot_machine_over.png')
+    this.load.image(TextureKey.SlotMachineUnder, 'image/slot/slot_machine_under.png')
+    this.load.image(TextureKey.SlotStartA, 'image/slot/button/slot_start_a.png')
+    this.load.image(TextureKey.SlotStartB, 'image/slot/button/slot_start_b.png')
+    this.load.image(TextureKey.SlotButtonA, 'image/slot/button/slot_button_a.png')
+    this.load.image(TextureKey.SlotButtonB, 'image/slot/button/slot_button_b.png')
+    this.load.image(TextureKey.SlotButtonC, 'image/slot/button/slot_button_c.png')
+    this.load.image(TextureKey.SlotButtonD, 'image/slot/button/slot_button_d.png')
+    this.load.image(TextureKey.SlotButtonE, 'image/slot/button/slot_button_e.png')
+    this.load.image(TextureKey.SlotButtonF, 'image/slot/button/slot_button_f.png')
+    this.load.image(TextureKey.SlotNumber0, 'image/slot/number/slot_number_0.png')
+    this.load.image(TextureKey.SlotNumber1, 'image/slot/number/slot_number_1.png')
+    this.load.image(TextureKey.SlotNumber2, 'image/slot/number/slot_number_2.png')
+    this.load.image(TextureKey.SlotNumber3, 'image/slot/number/slot_number_3.png')
+    this.load.image(TextureKey.SlotNumber4, 'image/slot/number/slot_number_4.png')
+    this.load.image(TextureKey.SlotNumber5, 'image/slot/number/slot_number_5.png')
+    this.load.image(TextureKey.SlotNumber6, 'image/slot/number/slot_number_6.png')
+    this.load.image(TextureKey.SlotNumber7, 'image/slot/number/slot_number_7.png')
+    this.load.image(TextureKey.SlotNumber8, 'image/slot/number/slot_number_8.png')
+    this.load.image(TextureKey.SlotNumber9, 'image/slot/number/slot_number_9.png')
   }
 
   create() {
