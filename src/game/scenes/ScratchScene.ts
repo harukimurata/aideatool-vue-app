28d5ac865446ba0609c74156d7630722d752b79c
harukimurata <havedream0108@gmail.com>
2024-10-26 15:14:28 +0900

    fix: スクラッチのひな型

--- src/game/scenes/ScratchScene.ts
+++ src/game/scenes/ScratchScene.ts
@@ -5,13 +5,18 @@ import SceneKey from '../const/SceneKey'
 import TextureKey from '../const/TextureKey'
 import ImageButton from '../components/ImageButton'
 
-const BASE_POS_X = 100
+const BASE_POS_X = 200
 const BASE_POS_Y = 100
 const BASE_WIDTH = 100
 const BASE_HEIGHT = 100
 const ADD_BASE_POS_X = 100
 const ADD_BASE_POS_Y = 100
 const NUM_IMAGES = 9
+
+interface IMAGE_POS {
+  x: number
+  y: number
+}
 
 export class ScratchScene extends Scene {
   private slotNumberArray: string[] = [
@@ -22,7 +27,11 @@ export class ScratchScene extends Scene {
     TextureKey.SlotNumber4 //申
   ]
 
+  private scratchText!: GameObjects.Text
   initButton!: ImageButton
+  private openScratchNum = NUM_IMAGES
+  private imagePos: IMAGE_POS[] = []
+  private selectButtonBox: ImageButton[] = []
   private imageBox: GameObjects.Image[] = []
   private numberBox: number[] = []
 
@@ -33,6 +42,20 @@ export class ScratchScene extends Scene {
   create() {
     const gameWidth = this.scale.width
     const gameHeight = this.scale.height
+
+    this.scratchText = this.add
+      .text(gameWidth / 2, gameHeight / 2 + 200, '当たり画像', {
+        fontFamily: 'Arial Black',
+        fontSize: 64,
+        color: '#ffffff',
+        stroke: '#000000',
+        strokeThickness: 8,
+        align: 'center'
+      })
+      .setAlpha(0)
+      .setScale(0.2)
+      .setOrigin(0.5)
+      .setDepth(100)
 
     this.add
       .text(gameWidth / 2, gameHeight / 2, 'ScratchScene', {
@@ -60,17 +83,36 @@ export class ScratchScene extends Scene {
 
     for (let i = 0; i < NUM_IMAGES; i++) {
       this.imageBox[i] = this.add.image(BASE_POS_X, BASE_POS_X, this.slotNumberArray[0])
+      this.selectButtonBox[i] = new ImageButton(
+        this,
+        BASE_POS_X,
+        BASE_POS_Y,
+        TextureKey.SlotStartA,
+        TextureKey.SlotStartB,
+        () => {
+          this.tweens.add({
+            targets: this.selectButtonBox[i],
+            alphaTopRight: { value: 0, duration: 300, ease: 'Power1' },
+            alphaBottomRight: { value: 0, duration: 300, ease: 'Power1' },
+            alphaTopLeft: { value: 0, duration: 200, ease: 'Power1', delay: 100 },
+            alphaBottomLeft: { value: 0, duration: 200, ease: 'Power1', delay: 100 }
+          })
+          this.openScratch()
+        }
+      ).setScale(0.6)
+      this.add.existing(this.selectButtonBox[i])
     }
 
     this.initNumberBox()
-
     EventBus.emit('current-scene-ready', this)
   }
 
+  //スクラッチ番号ボックスの初期化
   private initNumberBox() {
-    const maxNumber = this.weightedRandom()
-    console.log('maxNumber', maxNumber)
+    this.openScratchNum = NUM_IMAGES
 
+    this.scratchText.setAlpha(0).setScale(0.2)
+    const maxNumber = this.weightedRandom()
     for (let i = 0; i < NUM_IMAGES; i++) {
       if (i < maxNumber) {
         this.numberBox[i] = 0
@@ -81,22 +123,39 @@ export class ScratchScene extends Scene {
 
     this.numberBox = this.shuffleNumberBox(this.numberBox)
 
-    console.log('shuffle this.numberBox', this.numberBox)
-
     let boxNumber = 0
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        this.imageBox[boxNumber].setPosition(
-          BASE_POS_X + ADD_BASE_POS_X * i,
-          BASE_POS_Y + ADD_BASE_POS_Y * j
-        )
+        this.imagePos[boxNumber] = {
+          x: BASE_POS_X + ADD_BASE_POS_X * i,
+          y: BASE_POS_Y + ADD_BASE_POS_Y * j
+        }
+        this.imageBox[boxNumber].setPosition(BASE_POS_X, BASE_POS_Y)
         this.imageBox[boxNumber].setTexture(this.slotNumberArray[this.numberBox[boxNumber]])
 
+        this.selectButtonBox[boxNumber].setAlpha(1)
+        this.selectButtonBox[boxNumber].setPosition(BASE_POS_X, BASE_POS_Y)
         boxNumber++
       }
     }
+
+    this.initImagePos()
   }
 
+  //スクラッチ番号ボックスの移動後の場所
+  private initImagePos() {
+    for (let i = 0; i < NUM_IMAGES; i++) {
+      this.tweens.add({
+        targets: [this.imageBox[i], this.selectButtonBox[i]],
+        x: this.imagePos[i].x,
+        y: this.imagePos[i].y,
+        ease: 'Bounce',
+        duration: 500
+      })
+    }
+  }
+
+  //スクラッチ番号ボックスをランダムに並び替える
   private shuffleNumberBox(array: number[]) {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1))
@@ -105,6 +164,21 @@ export class ScratchScene extends Scene {
     return array
   }
 
+  //スクラッチ削り処理
+  private openScratch() {
+    this.openScratchNum--
+    if (this.openScratchNum <= 0) {
+      this.tweens.add({
+        targets: this.scratchText,
+        alpha: 1,
+        scale: 1,
+        ease: 'Bounce',
+        duration: 800
+      })
+    }
+  }
+
+  //あたりの発生確率を変える
   private weightedRandom() {
     const random = Math.random() // 0 <= random < 1
 
