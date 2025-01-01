47cb56b96f200fe647c4aea806c073f75542b89c
harukimurata <havedream0108@gmail.com>
2025-01-01 10:34:55 +0900

    modify: オブジェクトを配列で管理するようにする

--- src/game/scenes/SlotScene.ts
+++ src/game/scenes/SlotScene.ts
@@ -11,10 +11,19 @@ import { isEvenNumber } from '../utils'
 import { delayPromise } from '../helper'
 import { SPECIAL_HANDS } from '../const/SlotHand'
 
+const SLOT_REEL_NUM = 3
+const STOP_BUTTON_NUM = 3
 const MAX_SLOT_BET = 3
 const MAX_BONUS_STAR = 5
+const SLOT_REEL_POS_X = 170
+const SLOT_REEL_POS_X_SPACE = 170
+const STOP_BUTTON_POS_X = 120
+const STOP_BUTTON_POS_X_SPACE = 120
+const STOP_BUTTON_POS_Y = 70
 const BONUS_STAR_POS_X = 100
 const BONUS_STAR_POS_X_SPACE = 50
+const BET_LUMP_POS_X = 170
+const BET_LUMP_POS_X_SPACE = 170
 const ADD_DOUBLE_UP_CHANCE = 11
 const COIN_NUM_POS_X = 450
 const COIN_NUM_POS_Y = 135
@@ -34,20 +43,18 @@ export class SlotScene extends Scene {
   doubleChanceIcon!: GameObjects.Image
   doubleUpChanceNumText!: GameObjects.Text
 
-  slotReel1!: SlotReelNumber
-  slotReel2!: SlotReelNumber
-  slotReel3!: SlotReelNumber
-
   slotBetButton!: ImageButton
   slotStartButton!: ImageButton
+
+  slotReels: SlotReelNumber[] = []
+
+  slotStopButtons: ImageButton[] = []
+
   slotStopButton1!: ImageButton
   slotStopButton2!: ImageButton
   slotStopButton3!: ImageButton
 
-  betLamp1!: ImageManager
-  betLamp2!: ImageManager
-  betLamp3!: ImageManager
-
+  betLamps: ImageManager[] = []
   bonusStars: ImageManager[] = []
 
   private betCount = 0
@@ -75,66 +82,32 @@ export class SlotScene extends Scene {
       .image(gameWidth / 2, gameHeight / 2, TextureKey.SlotMachineUnder)
       .setScale(1.2)
 
-    this.slotReel1 = new SlotReelNumber(
-      this,
-      gameWidth / 2 - 170,
-      gameHeight / 2 - SLOT_MACHINE_NUMBER_Y_POS,
-      TextureKey.SlotNumber0,
-      gameWidth,
-      gameHeight
-    ).setScale(1.5)
-    this.add.existing(this.slotReel1)
-
-    this.slotReel2 = new SlotReelNumber(
-      this,
-      gameWidth / 2,
-      gameHeight / 2 - SLOT_MACHINE_NUMBER_Y_POS,
-      TextureKey.SlotNumber0,
-      gameWidth,
-      gameHeight
-    ).setScale(1.5)
-    this.add.existing(this.slotReel2)
-
-    this.slotReel3 = new SlotReelNumber(
-      this,
-      gameWidth / 2 + 170,
-      gameHeight / 2 - SLOT_MACHINE_NUMBER_Y_POS,
-      TextureKey.SlotNumber0,
-      gameWidth,
-      gameHeight
-    ).setScale(1.5)
-    this.add.existing(this.slotReel3)
+    for (let i = 0; i < SLOT_REEL_NUM; i++) {
+      this.slotReels[i] = new SlotReelNumber(
+        this,
+        gameWidth / 2 - SLOT_REEL_POS_X + SLOT_REEL_POS_X_SPACE * i,
+        gameHeight / 2 - SLOT_MACHINE_NUMBER_Y_POS,
+        TextureKey.SlotNumber0,
+        gameWidth,
+        gameHeight
+      ).setScale(1.5)
+      this.add.existing(this.slotReels[i])
+    }
 
     this.slotMachineOver = this.add
       .image(gameWidth / 2, gameHeight / 2, TextureKey.SlotMachineOver)
       .setScale(1.2)
 
-    this.betLamp1 = new ImageManager(
-      this,
-      gameWidth / 2 - 170,
-      180,
-      [TextureKey.SlotButtonC, TextureKey.SlotButtonD],
-      0.6
-    )
-    this.add.existing(this.betLamp1)
-
-    this.betLamp2 = new ImageManager(
-      this,
-      gameWidth / 2,
-      180,
-      [TextureKey.SlotButtonC, TextureKey.SlotButtonD],
-      0.6
-    )
-    this.add.existing(this.betLamp2)
-
-    this.betLamp3 = new ImageManager(
-      this,
-      gameWidth / 2 + 170,
-      180,
-      [TextureKey.SlotButtonC, TextureKey.SlotButtonD],
-      0.6
-    )
-    this.add.existing(this.betLamp3)
+    for (let i = 0; i < MAX_SLOT_BET; i++) {
+      this.betLamps[i] = new ImageManager(
+        this,
+        gameWidth / 2 - BET_LUMP_POS_X + BET_LUMP_POS_X_SPACE * i,
+        180,
+        [TextureKey.SlotButtonC, TextureKey.SlotButtonD],
+        0.6
+      )
+      this.add.existing(this.betLamps[i])
+    }
 
     for (let i = 0; i < MAX_BONUS_STAR; i++) {
       this.bonusStars[i] = new ImageManager(
@@ -222,71 +195,34 @@ export class SlotScene extends Scene {
     ).setScale(0.7)
     this.add.existing(this.slotStartButton)
 
-    this.slotStopButton1 = new ImageButton(
-      this,
-      gameWidth / 2 - 120,
-      gameHeight / 2 + 70,
-      TextureKey.SlotButtonA,
-      TextureKey.SlotButtonB,
-      () => {
-        if (this.isStartReel && !this.slotReel1.getIsStop()) {
-          this.slotReel1.setIsStop(true)
+    for (let i = 0; i < STOP_BUTTON_NUM; i++) {
+      this.slotStopButtons[i] = new ImageButton(
+        this,
+        gameWidth / 2 - STOP_BUTTON_POS_X + STOP_BUTTON_POS_X_SPACE * i,
+        gameHeight / 2 + STOP_BUTTON_POS_Y,
+        TextureKey.SlotButtonA,
+        TextureKey.SlotButtonB,
+        () => {
+          if (this.isStartReel && !this.slotReels[i].getIsStop()) {
+            this.slotReels[i].setIsStop(true)
 
-          if (this.isChargeMax) {
-            this.slotReel1.setStopNumber(7)
+            if (this.isChargeMax) {
+              this.slotReels[i].setStopNumber(7)
+            }
+
+            this.slotResult()
+            this.slotReels[i].stopAnimation(this)
           }
-
-          this.slotResult()
-          this.slotReel1.stopAnimation(this)
         }
-      }
-    ).setScale(0.9)
-    this.add.existing(this.slotStopButton1)
-
-    this.slotStopButton2 = new ImageButton(
-      this,
-      gameWidth / 2,
-      gameHeight / 2 + 70,
-      TextureKey.SlotButtonA,
-      TextureKey.SlotButtonB,
-      () => {
-        if (this.isStartReel && !this.slotReel2.getIsStop()) {
-          this.slotReel2.setIsStop(true)
-
-          if (this.isChargeMax) {
-            this.slotReel2.setStopNumber(7)
-          }
-          this.slotResult()
-          this.slotReel2.stopAnimation(this)
-        }
-      }
-    ).setScale(0.9)
-    this.add.existing(this.slotStopButton2)
-
-    this.slotStopButton3 = new ImageButton(
-      this,
-      gameWidth / 2 + 120,
-      gameHeight / 2 + 70,
-      TextureKey.SlotButtonA,
-      TextureKey.SlotButtonB,
-      () => {
-        if (this.isStartReel && !this.slotReel3.getIsStop()) {
-          this.slotReel3.setIsStop(true)
-          if (this.isChargeMax) {
-            this.slotReel3.setStopNumber(7)
-          }
-          this.slotResult()
-          this.slotReel3.stopAnimation(this)
-        }
-      }
-    ).setScale(0.9)
-    this.add.existing(this.slotStopButton3)
+      ).setScale(0.9)
+      this.add.existing(this.slotStopButtons[i])
+    }
 
     EventBus.emit('current-scene-ready', this)
 
-    this.slotReel1.init(this)
-    this.slotReel2.init(this)
-    this.slotReel3.init(this)
+    for (let i = 0; i < SLOT_REEL_NUM; i++) {
+      this.slotReels[i].init(this)
+    }
   }
 
   /**
@@ -303,9 +239,9 @@ export class SlotScene extends Scene {
     }
 
     if (this.isStartReel) {
-      this.slotReel1.update()
-      this.slotReel2.update()
-      this.slotReel3.update()
+      this.slotReels[0].update()
+      this.slotReels[1].update()
+      this.slotReels[2].update()
     }
   }
 
@@ -320,11 +256,11 @@ export class SlotScene extends Scene {
       this.coinNumText.setText('COIN: ' + this.coinNum)
 
       if (this.betCount == 1) {
-        this.betLamp1.addCount()
+        this.betLamps[0].addCount()
       } else if (this.betCount == 2) {
-        this.betLamp2.addCount()
+        this.betLamps[1].addCount()
       } else if (this.betCount == 3) {
-        this.betLamp3.addCount()
+        this.betLamps[2].addCount()
       }
     }
   }
@@ -344,7 +280,11 @@ export class SlotScene extends Scene {
    * @param object
    */
   private slotResult() {
-    if (this.slotReel1.getIsStop() && this.slotReel2.getIsStop() && this.slotReel3.getIsStop()) {
+    if (
+      this.slotReels[0].getIsStop() &&
+      this.slotReels[1].getIsStop() &&
+      this.slotReels[2].getIsStop()
+    ) {
       if (this.isChargeMax) {
         this.starChargeCountDowner()
       } else {
@@ -352,9 +292,9 @@ export class SlotScene extends Scene {
       }
       this.starImageManager()
       const addCoin = calcSlotHand(
-        this.slotReel1.getStopNumber(),
-        this.slotReel2.getStopNumber(),
-        this.slotReel3.getStopNumber()
+        this.slotReels[0].getStopNumber(),
+        this.slotReels[1].getStopNumber(),
+        this.slotReels[2].getStopNumber()
       )
       if (addCoin > 0) {
         this.calcSpecialHands(addCoin)
@@ -374,9 +314,9 @@ export class SlotScene extends Scene {
         this.isReplay = true
         this.betCount = 3
         this.betNumText.setText('Bet: ' + this.betCount * 2)
-        this.betLamp1.setImage(1)
-        this.betLamp2.setImage(1)
-        this.betLamp3.setImage(1)
+        for (let i = 0; i < MAX_SLOT_BET; i++) {
+          this.betLamps[i].setImage(1)
+        }
         break
 
       case SPECIAL_HANDS.DOUBLE_UP:
@@ -445,9 +385,9 @@ export class SlotScene extends Scene {
   private starCharger() {
     if (
       isEvenNumber(
-        this.slotReel1.getStopNumber(),
-        this.slotReel2.getStopNumber(),
-        this.slotReel3.getStopNumber()
+        this.slotReels[0].getStopNumber(),
+        this.slotReels[1].getStopNumber(),
+        this.slotReels[2].getStopNumber()
       )
     ) {
       this.chargeStarCount++
@@ -494,21 +434,25 @@ export class SlotScene extends Scene {
    * スロット初期化
    */
   private initSlot() {
-    if (this.slotReel1.getIsStop() && this.slotReel2.getIsStop() && this.slotReel3.getIsStop()) {
+    if (
+      this.slotReels[0].getIsStop() &&
+      this.slotReels[1].getIsStop() &&
+      this.slotReels[2].getIsStop()
+    ) {
       this.time.delayedCall(500, () => {
         if (!this.isReplay) {
-          this.betLamp1.setInit()
-          this.betLamp2.setInit()
-          this.betLamp3.setInit()
+          for (let i = 0; i < MAX_SLOT_BET; i++) {
+            this.betLamps[i].setInit()
+          }
           this.betCount = 0
           this.isBet = false
           this.addCoinNum = 0
           this.betNumText.setText('Bet: ' + this.betCount)
         }
 
-        this.slotReel1.init(this)
-        this.slotReel2.init(this)
-        this.slotReel3.init(this)
+        for (let i = 0; i < SLOT_REEL_NUM; i++) {
+          this.slotReels[i].init(this)
+        }
         this.isStartReel = false
 
         this.doubleUpChanceCount = this.doubleUpChanceCount - 1
