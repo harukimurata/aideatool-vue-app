d1b4d234d158e6eb068ba3c140d9acc86145acfd
harukimurata <havedream0108@gmail.com>
2025-11-02 15:58:17 +0900

    modify: デバッグテキストのロジックを変更

--- src/game/components/DebugTexts.ts
+++ src/game/components/DebugTexts.ts
@@ -1,16 +1,29 @@
 import UiText from './UiText'
 
 export default class DebugTexts {
-  private texts!: UiText[]
+  private texts!: {
+    baseText: string
+    uiText: UiText
+  }[]
 
   constructor() {
     this.texts = []
   }
 
-  public init(scene: Phaser.Scene, text: string) {
-    const y = 10 + this.texts.length * 22
-    const uiText = new UiText(scene, 10, y, text, 'Arial', '20px', '#000000', 100)
-    this.texts.push(uiText)
+  /**
+   * デバッグ用テキストの初期化
+   * {1}を変数部分として扱うことができる
+   * @param scene
+   * @param texts
+   */
+  public init(scene: Phaser.Scene, texts: string[]) {
+    let index = 0
+    for (const text of texts) {
+      const y = 10 + this.texts.length * 22
+      const uiText = new UiText(scene, 10, y, text, 'Arial', '20px', '#000000', 100)
+      this.texts.push({ baseText: text, uiText })
+      index++
+    }
   }
 
   /**
@@ -20,7 +33,20 @@ export default class DebugTexts {
    */
   public setTextString(index: number, text: string) {
     if (this.texts[index]) {
-      this.texts[index].setTextString(text)
+      this.texts[index].uiText.setTextString(text)
+    }
+  }
+
+  /**
+   * 指定した番号のテキストの変数部分を置換する
+   * @param index
+   * @param variable
+   */
+  public replaceVariable(index: number, variable: string | number) {
+    if (this.texts[index]) {
+      const nowText = this.texts[index].baseText
+      const newText = nowText.replace('{1}', variable.toString())
+      this.texts[index].uiText.setTextString(newText)
     }
   }
 }
