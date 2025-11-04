9ba6effb16972811cee9fd89e75ded6cc6a267e9
harukimurata <havedream0108@gmail.com>
2025-11-04 23:13:06 +0900

    modify: 空気抵抗を受ける矢の挙動

--- src/game/logic/physics.ts
+++ src/game/logic/physics.ts
@@ -1,4 +1,13 @@
 const GRAVITY = 9.8 // m/s^2
+const STEP_TIME = 0.001 // 計算刻み時間 s
+
+// 状態を保持する構造体
+export interface ArrowState {
+  z: number // 飛距離 [m]
+  y: number // 高度 [m]
+  vz: number // 水平方向速度 [m/s]
+  vy: number // 鉛直方向速度 [m/s]
+}
 
 /**
  * 加速度の算出 v2-v1/time
@@ -25,23 +34,23 @@ export function deceleration(v1: number, v2: number, time: number): number {
 /**
  * 弓の弾性エネルギーの算出 1/2*k*x^2
  * @param k ばね定数
- * @param x 弓を引いた距離
+ * @param d 弓を引いた距離
  * @returns
  */
-export function arrowElasticity(k: number, x: number): number {
-  return (1 / 2) * k * x * x
+export function arrowElasticity(k: number, d: number): number {
+  return (1 / 2) * k * d * d
 }
 
 /**
- * 矢の初速度の算出 sqrt(eta*k*x^2/m)
+ * 矢の初速度の算出 sqrt(eta*k*d^2/m)
  * @param k ばね定数
- * @param x 弓を引いた距離
+ * @param d 弓を引いた距離
  * @param m 矢の重さ
  * @param eta エネルギー変換効率（0 < eta ≤ 1）
  * @returns
  */
-export function arrowInitialVelocity(k: number, x: number, m: number, eta: number = 1.0): number {
-  return Math.sqrt((2 * eta * arrowElasticity(k, x)) / m)
+export function arrowInitialVelocity(k: number, d: number, m: number, eta: number = 1.0): number {
+  return Math.sqrt((2 * eta * arrowElasticity(k, d)) / m)
 }
 
 /**
@@ -56,4 +65,66 @@ export function arrowFlightDistance(k: number, x: number, m: number, thetaDeg: n
   const v_0 = arrowInitialVelocity(k, x, m)
   const theta = (thetaDeg * Math.PI) / 180
   return (v_0 * v_0 * theta) / GRAVITY
+}
+
+/**
+ * 空気抵抗の算出
+ * @param cd // 空気抵抗係数
+ * @param csa // 断面積
+ * @param ad // 空気の密度
+ * @param v // 速度
+ * @returns
+ */
+export function airResistance(cd: number, csa: number, ad: number, v: number): number {
+  return 0.5 * cd * ad * csa * v * v
+}
+
+/**
+ * 抗力による加速度の算出
+ * @param Fd // 抗力
+ * @param m // 質量
+ * @param d // 速度成分(方向)
+ * @param v // 速度の大きさ
+ * @returns
+ */
+export function resistanceAcceleration(Fd: number, m: number, d: number, v: number): number {
+  return -(Fd / m) * (d / v)
+}
+
+/**
+ * 空気抵抗を考慮した矢の飛行状態の1ステップ進行
+ * @param state {z, y, vz, vy} 矢の状態
+ * @param m // 矢の重さ
+ * @param cd // 空気抵抗係数
+ * @param csa // 矢の断面積
+ * @param ad // 空気の密度
+ * @returns {z, y, vz, vy} 矢の状態
+ */
+export function stepAirResistanceArrowFlight(
+  state: ArrowState,
+  m: number,
+  cd: number,
+  csa: number,
+  ad: number,
+  dt: number = STEP_TIME
+): ArrowState {
+  const { z, y, vz, vy } = state
+
+  const v = Math.sqrt(vz * vz + vy * vy) // 速度の大きさ
+  const Fd = airResistance(cd, csa, ad, v) // 空気抵抗
+  const ax = resistanceAcceleration(Fd, m, vz, v) // 抗力によるz方向加速度
+  const ay = resistanceAcceleration(Fd, m, vy, v) - GRAVITY // 抗力＋重力によるy方向加速度
+
+  // 次ステップの速度と位置
+  const newVz = vz + ax * dt
+  const newVy = vy + ay * dt
+  const newZ = z + newVz * dt
+  const newY = y + newVy * dt
+
+  return {
+    z: newZ,
+    y: Math.max(0, newY), // 地面以下は0に固定
+    vz: newVz,
+    vy: newVy
+  }
 }
--- src/game/scenes/PinpointShooterScene.ts
+++ src/game/scenes/PinpointShooterScene.ts
@@ -5,12 +5,22 @@ import { SceneKey, SceneKeyIndex } from '../const/SceneKey'
 import TextureKey from '../const/TextureKey'
 import ImageButton from '../components/ImageButton'
 import DebugTexts from '../components/DebugTexts'
-import { arrowFlightDistance } from '../logic/physics'
+import {
+  type ArrowState,
+  arrowInitialVelocity,
+  stepAirResistanceArrowFlight
+} from '../logic/physics'
 
 const BOW_SPRING_CONSTANT = 300 // N/m
 const ARROW_MASS = 0.05 // kg
 const BOW_DRAW_DISTANCE = 0.5 // m 変数で決めるようにする
 const SHOOTING_ANGLE_DEG = 45 // degrees 変数で決めるようにする
+const START_ARROW_HEIGHT = 1.5 // m
+const AIR_RESISTANCE_COEFFICIENT = 1.0 // 空気抵抗係数
+const ARROW_CROSS_SECTIONAL_AREA = 0.0005 // m^2
+const AIR_DENSITY = 1.225 // kg/m^3
+const GRAVITY = 9.8 // m/s^2
+const MOVE_SPEED = 45 // km/h
 
 export class PinpointShooterScene extends Scene {
   // ゲーム内時間
@@ -26,6 +36,13 @@ export class PinpointShooterScene extends Scene {
 
   slotResetButton!: ImageButton
   slotPauseButton!: ImageButton
+
+  arrowState: ArrowState = {
+    z: 0,
+    y: START_ARROW_HEIGHT,
+    vz: 0,
+    vy: 0
+  }
 
   constructor() {
     super(SceneKey[SceneKeyIndex.PinpointShooterScene].scene_name)
@@ -46,18 +63,18 @@ export class PinpointShooterScene extends Scene {
     this.debugTexts = new DebugTexts()
     this.debugTexts.init(this, [
       'Pinpoint Shooter Scene',
-      'World Time: {1} sec',
-      'arrowFlightDistance: {1} m'
+      'arrowFlightDistance: {1}',
+      'px arrowFlightDistance: {1}'
     ])
 
     this.slotResetButton = new ImageButton(
       this,
       this.gameWidth / 2,
       this.gameHeight / 2,
-      TextureKey.SlotStartA,
-      TextureKey.SlotStartB,
+      TextureKey.SlotBetA,
+      TextureKey.SlotBetB,
       () => {
-        this.worldReset()
+        this.reset()
       }
     ).setScale(0.7)
     this.add.existing(this.slotResetButton)
@@ -69,32 +86,78 @@ export class PinpointShooterScene extends Scene {
       TextureKey.SlotStartA,
       TextureKey.SlotStartB,
       () => {
-        this.worldStop()
+        this.start()
       }
     ).setScale(0.7)
     this.add.existing(this.slotPauseButton)
 
-    const flightDistance = arrowFlightDistance(
-      BOW_SPRING_CONSTANT,
-      BOW_DRAW_DISTANCE,
-      ARROW_MASS,
-      SHOOTING_ANGLE_DEG
+    //矢の初期化
+    this.initArrowState()
+    this.debugTexts.replaceVariable(
+      1,
+      `z: ${this.arrowState.z.toFixed(2)}, y: ${this.arrowState.y.toFixed(
+        2
+      )}, vz: ${this.arrowState.vz.toFixed(2)}, vy: ${this.arrowState.vy.toFixed(2)}`
     )
-    this.debugTexts.replaceVariable(2, flightDistance.toFixed(2))
   }
 
   update(time: number, delta: number): void {
-    if (!this.stop) {
-      const worldTime = (this.time.now - this.worldTime) / 1000
-      this.debugTexts.replaceVariable(1, worldTime.toFixed(1))
+    if (this.stop) {
+      const dt = delta / 1000 // Phaser のフレーム時間
+      this.arrowState = stepAirResistanceArrowFlight(
+        this.arrowState,
+        ARROW_MASS,
+        AIR_RESISTANCE_COEFFICIENT,
+        ARROW_CROSS_SECTIONAL_AREA,
+        AIR_DENSITY,
+        dt
+      )
+
+      this.debugTexts.replaceVariable(
+        1,
+        `z: ${this.arrowState.z.toFixed(2)}, y: ${this.arrowState.y.toFixed(
+          2
+        )}, vz: ${this.arrowState.vz.toFixed(2)}, vy: ${this.arrowState.vy.toFixed(2)}`
+      )
+
+      this.debugTexts.replaceVariable(
+        2,
+        `z: ${(this.arrowState.z * 10).toFixed(2)}, y: ${(this.arrowState.y * 10).toFixed(2)}`
+      )
+
+      if (this.arrowState.y <= 0) {
+        this.stop = false
+      }
     }
   }
 
-  public worldReset() {
-    this.worldTime = this.time.now
+  private reset() {
+    this.initArrowState()
+    this.debugTexts.replaceVariable(
+      1,
+      `z: ${this.arrowState.z.toFixed(2)}, y: ${this.arrowState.y.toFixed(
+        2
+      )}, vz: ${this.arrowState.vz.toFixed(2)}, vy: ${this.arrowState.vy.toFixed(2)}`
+    )
   }
 
-  public worldStop() {
-    this.stop = !this.stop
+  private start() {
+    this.stop = true
+  }
+
+  /**
+   * 矢の状態の初期化
+   */
+  private initArrowState() {
+    const v_0 = arrowInitialVelocity(BOW_SPRING_CONSTANT, BOW_DRAW_DISTANCE, ARROW_MASS)
+
+    const theta = (SHOOTING_ANGLE_DEG * Math.PI) / 180
+
+    this.arrowState.z = 0
+    this.arrowState.y = START_ARROW_HEIGHT
+    this.arrowState.vz = v_0 * Math.cos(theta)
+    this.arrowState.vy = v_0 * Math.sin(theta)
+
+    console.log(this.arrowState)
   }
 }
